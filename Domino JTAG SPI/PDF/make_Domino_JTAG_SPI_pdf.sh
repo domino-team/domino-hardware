a14feade55d4f2db551ab3a9e4c3c0cb0498e009
Michel Stempin <michel.stempin@wanadoo.fr>
2014-08-22 08:01:06 +0200

    Domino JTAG/SPI tile Revision B

---
+++ Domino JTAG SPI/PDF/make_Domino_JTAG_SPI_pdf.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pdftk Domino\ JTAG\ SPI_SCH.pdf Domino\ JTAG\ SPI_CMP.pdf Domino\ JTAG\ SPI_SOL.pdf Domino\ JTAG\ SPI_PLC.pdf Domino\ JTAG\ SPI_PLS.pdf Domino\ JTAG\ SPI_STC.pdf Domino\ JTAG\ SPI_STS.pdf Domino\ JTAG\ SPI_CRC.pdf Domino\ JTAG\ SPI_CRS.pdf Domino\ JTAG\ SPI_DRD.pdf Domino\ JTAG\ SPI_ASC.pdf Domino\ JTAG\ SPI_ASS.pdf Domino\ JTAG\ SPI_OLN.pdf Domino\ JTAG\ SPI_PIN.pdf Domino\ JTAG\ SPI_BOM.pdf cat output Domino\ JTAG\ SPI.pdf
+
--- Domino JTAG SPI/PDF/make_Domino_JTAG_pdf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pdftk Domino\ JTAG_SCH.pdf Domino\ JTAG_CMP.pdf Domino\ JTAG_SOL.pdf Domino\ JTAG_PLC.pdf Domino\ JTAG_PLS.pdf Domino\ JTAG_STC.pdf Domino\ JTAG_STS.pdf Domino\ JTAG_CRC.pdf Domino\ JTAG_CRS.pdf Domino\ JTAG_DRD.pdf Domino\ JTAG_ASC.pdf Domino\ JTAG_ASS.pdf Domino\ JTAG_OLN.pdf Domino\ JTAG_PIN.pdf Domino\ JTAG_BOM.pdf cat output Domino\ JTAG.pdf
-
